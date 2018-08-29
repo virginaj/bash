7543b34dc59a8a842548d1c7df0fd4b46af588f2
virginaj <vajacob@comcast.net>
2018-08-29 14:33:02 -0500

    MySecond lab

--- MyLab02.sh
+++ MyLab02.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-grep nobody /etc/passwd
+##My second lab
+#grep nobody /etc/passwd
+#echo $?
 
-echo $?
+echo -n "please under the name"
+read user
+grep $user /etc/passwd
 
-if [$? -eq -1]; "user was found"; fi
+if [ $? -eq 0 ]; then
+    echo "user was found"
+else
+    echo "User not found"
+ fi
 
+#2
+ls /etc/*| grep ^[0-9]
 
-ls /etc/passwd | grep [0-9]$
-
-find /etc
-
+#3
 wget -q0-www.yelp.com | grep yelp | wc -1
 
+#4
 ls > filelist 
 
+find . -mtime +10 # modify 10 days
